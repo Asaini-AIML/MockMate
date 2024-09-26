64cb7c6409ac5840f1120a0b343952f1555774a1
Anubhav Saini <anubhav.21b1531122@abes.ac.in>
2024-09-27 00:10:10 +0530

    Input Page set

--- mockmate/app/dashboard/_components/AddNewInterview.jsx
+++ mockmate/app/dashboard/_components/AddNewInterview.jsx
@@ -11,8 +11,17 @@ import {
 import { Input } from '/components/ui/input.jsx'
  import { useState } from 'react'
   import { Button } from '/components/ui/button.jsx'
+  import { Textarea } from '/components/ui/textarea.jsx'
 function AddNewInterview() {
     const [openDialog, setOpenDailog] = useState(false)
+    const[jobPosition,setJobPosition] = useState('')
+    const[jobDesc,setJobDesc] = useState('')
+    const[jobExperience,setJobExperience] = useState('')
+
+    const onSubmit=(e)=>{
+        e.preventDefault();
+   console.log(jobPosition,jobDesc,jobExperience)
+    }
   return (
     <div>
         <div className='p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-sm cursor-pointer transition-all' onClick={()=>setOpenDailog(true)}>
@@ -26,17 +35,27 @@ function AddNewInterview() {
     <DialogHeader>
       <DialogTitle className='text-2xl'>Tell us more about your job interviwing </DialogTitle>
       <DialogDescription>
+        <form onSubmit={onSubmit}>
         <div className=''>
           <h2>add Details about your job Position/role,job description and experience</h2>
-          <div className='my-2 mt-7 '>
+          <div className='my-3 mt-7 '>
             <label>Job Role/Jop Position</label>
-            <Input placeholder='Ex. Full Stack Devlopher'/>
+            <Input placeholder='Ex. Full Stack Devlopher' required onChange={(event)=>setJobPosition(event.target.value)}/>
+          </div>
+          <div className='my-3  '>
+            <label>Job Description/Tech Stack (In Short)</label>
+            <Textarea placeholder='Ex. React,Angular, Nodejs' required onChange={(event)=>setJobDesc(event.target.value)} />
+          </div>
+          <div className='my-3  '>
+            <label>years of experience</label>
+            <Input placeholder='Ex. 5' type="number" max="100" required onChange={(event)=>setJobExperience(event.target.value)}/>
           </div>
         </div>
         <div className='flex gap-5 justify-end'>
-            <Button variant="ghost" onClick={()=>setOpenDailog(false)}>Cancle</Button>
-            <Button>Start Interview</Button>
+            <Button type="button" variant="ghost" onClick={()=>setOpenDailog(false)}>Cancle</Button>
+            <Button type="submit">Start Interview</Button>
         </div>
+        </form>
       </DialogDescription>
     </DialogHeader>
   </DialogContent>
